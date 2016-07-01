a6eeb3691f73673deffae261fddd5e113bb03c08
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-07-01 00:01:57 -0700

    Make it work

--- app/app.js
+++ app/app.js
@@ -1,85 +1,139 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXVjaGVuYmVyZyIsImEiOiJjaWhtazd2b3QwMGY1dXNrcTd5N3V6NmM4In0.IjgFjyBRjdmUdi2MxSlw2w';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/auchenberg/ciq3detb8006icam106pxizsd',
+    center: [-68.13734351262877, 45.137451890638886],
+    minZoom: 1,
+    zoom: 1,
+    maxZoom: 5
+});
 
-function initAutocomplete() {
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-
-
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
+map.on('load', function () {
+    map.addSource('countries', {
+        'type': 'geojson',
+        'data': '/countries.geo.json'
     });
-    markers = [];
 
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+    map.addLayer({
+        "id": "country-borders",
+        "type": "line",
+        "source": "countries",
+        "layout": {},
+        "paint": {
+            "line-color": "#627BC1",
+            "line-width": 1
+        }
+    });
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
+    map.addLayer({
+        'id': 'country-fills',
+        'type': 'fill',
+        'source': 'countries',
+        'layout': {},
+        'paint': {
+            'fill-color': '#627BC1',
+            'fill-opacity': 0.2
+        }
+    });
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
+    map.addLayer({
+        "id": "country-hover",
+        "type": "fill",
+        "source": "countries",
+        "layout": {},
+        "paint": {
+            "fill-color": "#627BC1",
+            "fill-opacity": 1
+        },
+        "filter": ["==", "ADMIN", ""]
+    });
+
+    map.addLayer({
+        'id': 'country-visited',
+        'type': 'fill',
+        'source': 'countries',
+        'layout': {},
+        'paint': {
+            'fill-color': '#f44336',
+            'fill-opacity': 0.5
+        },
+        'filter' : ["in", "ADMIN", ""]
+    });
+
+
+
+   map.on("mousemove", function(e) {
+        var features = map.queryRenderedFeatures(e.point, { layers: ["country-fills"] });
+        if (features.length) {
+            map.setFilter("country-hover", ["==", "ADMIN", features[0].properties.ADMIN]);
+        } else {
+            map.setFilter("country-hover", ["==", "ADMIN", ""]);
+        }
+    });    
+
+    map.on("click", function(e) {
+
+      var features = map.queryRenderedFeatures(e.point, { layers: ["country-fills"] });
+
+      if (features.length) {
+
+        var currentCountry = features[0].properties.ADMIN
+        var filter = map.getFilter("country-visited");
+        var highlightedCountries;
+
+        if(filter && filter.length) {
+          var existingCountries = filter.slice(2)
+
+          if(existingCountries.indexOf(currentCountry) > -1) {
+            // Remove
+            highlightedCountries = existingCountries.filter(function(c) {
+              return c !== currentCountry
+            })
+          } else {
+            // Add
+            highlightedCountries = existingCountries.concat([currentCountry])
+          }
+
+        } else {
+          highlightedCountries = [currentCountry]
+        }
+
+        map.setFilter("country-visited", ["in", "ADMIN"].concat(highlightedCountries));
       }
-    });
-    map.fitBounds(bounds);
-  });
 
-  var world_geometry = new google.maps.FusionTablesLayer({
-    query: {
-      select: 'geometry',
-      from: '1N2LBk4JHwWpOY4d9fobIn27lfnZ5MDy-NoqqRpk'
-    },
-    map: map,
-    suppressInfoWindows: true
+
+    })
+
+});
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCPSaBT7rQHGwnfnl1cS7sjSGOoNWISMn0",
+  authDomain: "visited-c083d.firebaseapp.com",
+  databaseURL: "https://visited-c083d.firebaseio.com",
+  storageBucket: "visited-c083d.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+
+function login() {
+  var provider = new firebase.auth.FacebookAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+
+    console.log('user', user)
+    // ...
+  }).catch(function(error) {
+    console.log('error', error)
   });
 
 }
 
-
-// // Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyCPSaBT7rQHGwnfnl1cS7sjSGOoNWISMn0",
-//   authDomain: "visited-c083d.firebaseapp.com",
-//   databaseURL: "https://visited-c083d.firebaseio.com",
-//   storageBucket: "visited-c083d.appspot.com",
-// };
-// firebase.initializeApp(config);
+document.querySelector('.sign-in').addEventListener('click', function() {
+  login()
+}, false)
